b0f97a10786585a45058fb66fe5c6ed1130d8469
appreciated <goebeljohannes@gmx.net>
2017-04-21 21:59:22 +0200

    inital commit (circular progreessbar still needs to by stylable)

---
+++ circular-progressbar-demo/src/main/java/com/github/appreciated/demo/DemoUI.java
@@ -0,0 +1,42 @@
+package com.github.appreciated.demo;
+
+import com.github.appreciated.circular_progressbar.CircularProgressBar;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
+
+@Theme("demo")
+@Title("CircularProgressbar Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI {
+
+    private CircularProgressBar component;
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        // Initialize our new UI component
+        component = new CircularProgressBar();
+        // Show it in the middle of the screen
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        layout.setMargin(false);
+        layout.setSpacing(false);
+        layout.addComponent(component);
+        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+        setContent(layout);
+        component.setProgress(100);
+    }
+}
